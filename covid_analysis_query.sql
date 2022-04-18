cdda42628b5a00218a069046a66dfe6fdd41636c
mosimen <97052635+mosimen@users.noreply.github.com>
2022-04-18 19:01:14 +0100

    Update covid_analysis_query.sql

--- covid_analysis_query.sql
+++ covid_analysis_query.sql
@@ -3,58 +3,82 @@ use portfolio_project
 select location,date,total_cases,total_deaths,population
 from covid_deaths
 
+---We need to change the datatype of the data
+alter table covid_deaths
+alter column date date
+go
+
+alter table covid_deaths
+alter column population float
+go
+
+alter table covid_deaths
+alter column new_cases float
+go
+
+alter table covid_deaths
+alter column new_deaths float
+go
+
+alter table covid_deaths
+alter column total_cases float
+go
+
+alter table covid_deaths
+alter column total_deaths float
+go
 
 --- Percentage of poeple that died compared to the number of total cases
 select 
 	location,
-	cast(date as date) as date,
+	date,
 	total_cases,
 	new_cases,
 	total_deaths,
-	cast(total_deaths as float)/nullif(cast(total_cases as float),0) * 100 as death_percentage
+	total_deaths/nullif(total_cases ,0) * 100 as death_percentage
 from covid_deaths
 order by 1,2
 
 --- Death percentage in the US
-select
-	location,cast(date as date) as date, 
+select location, 
+	date, 
 	total_cases,
 	new_cases,
 	total_deaths, 
-	cast(total_deaths as float)/nullif(cast(total_cases as float),0) * 100 as death_percentage
+	(total_deaths/nullif(total_cases,0)) * 100 as death_percentage
 from covid_deaths
 where location like '%states%'
 order by 1,2
 
 --- Death percentage in nigeria
-select
-	location,cast(date as date) as date, 
+select location,
+	date, 
 	total_cases,
 	new_cases,
 	total_deaths, 
-	cast(total_deaths as float)/nullif(cast(total_cases as float),0) * 100 as death_percentage
+	(total_deaths /nullif(total_cases,0)) * 100 as death_percentage
 from covid_deaths
 where location like '%nigeria%'
 
 --- date when highest new cases was recorded in the us
-select
-	location,cast(date as date) as date, 
+select location,
+	date, 
 	total_cases,
 	new_cases,
 	total_deaths, 
-	cast(total_deaths as float)/nullif(cast(total_cases as float),0) * 100 as death_percentage
+	(total_deaths /nullif(total_cases,0)) * 100 as death_percentage
 from covid_deaths
 where location like '%states%'
 order by 4 desc
 
 
 --- date when highest new cases was recorded in nigeria
-select
-	location,cast(date as date) as date, 
+select location,
+	date,
 	total_cases,
 	new_cases,
 	total_deaths, 
-	cast(total_deaths as float)/nullif(cast(total_cases as float),0) * 100 as death_percentage
+	(total_deaths/nullif(total_cases,0)) * 100 as death_percentage
 from covid_deaths
 where location like '%nigeria%'
 order by 4 desc
@@ -63,18 +87,18 @@ order by 4 desc
 
 select
 	location,
-	cast(date as date) date,
+	date,
 	population,
 	total_cases,
-	(cast(total_cases as float)/ nullif(cast(population as float),0)) * 100 as cases_by_population
+	(total_cases / nullif(population,0)) * 100 as cases_by_population
 from [dbo].[covid_deaths]
 
 order by 1,2
 
 ---Country with highest infection rate
 select location,
-	cast(population as float) as population,
-	max(cast(total_cases as float)) as HighestInfection
+	population,
+	max(total_cases) as HighestInfection
 from
 	covid_deaths
 where continent != ' '
@@ -82,35 +106,30 @@ group by location, population
 order by HighestInfection desc
 
 --- The country with the highest infection rate compared to their total population
-
 select
 	location,
-	cast(population as float) as population,
-	max(cast(total_deaths as float))as total_deaths,
-	max(cast(total_cases as float)) as HighestInfectionCount,
-	max((cast(total_cases as float)/nullif(cast(population as float), 0))*100) as PercentPopulationInfected
+	population,
+	max(total_cases) as HighestInfectionCount,
+	max(total_cases/nullif(population, 0)*100) as PercentPopulationInfected
 from 
 covid_deaths
 where continent != ' '
 group by location, population
-order by 5 desc
+order by 4 desc
 
 ---Countries with the highest Death count
-
 select
-	location, population, max(cast(total_deaths as float)) as HighestDeathCount
+	location, population, max(total_deaths) as HighestDeathCount
 from covid_deaths
 where continent != ' '
 group  by location, population
 order by 3 desc
 
 ---Countries with the highest Death count per population
-
 select location,
-	cast(population as float) as population,
-	max(cast(total_deaths as float)) as HighestDeathCount,
-	max((cast(total_deaths as float)/nullif(cast(population as float),0))*100) as PercentPopulationDead
-
+	population,
+	max(total_deaths) as HighestDeathCount,
+	max((total_deaths /nullif(population,0))*100) as PercentPopulationDead
 from
 	covid_deaths
 where continent != ' '
@@ -120,46 +139,38 @@ order by 4 desc
 
 --- Highest Death count by continent
 select continent,
-	max(cast(total_deaths as float)) as HighestDeathCount
+	max(total_deaths) as HighestDeathCount
 from
 	covid_deaths
 where continent != ' '
 group by continent
 order by HighestDeathCount desc
 
----Global Death vs Infected
-select distinct(location),
-	max(cast(total_cases as float)) as total_cases,
-	max(cast(total_deaths as float)) as total_deaths,
-	(max(cast(total_deaths as float))/nullif(max(cast(total_cases as float)),0))*100 as PercentDeathvsInfected
-from covid_deaths
-where continent != ' '
-group by location
-
----
-select distinct(location),
-	sum(cast(new_cases as float)) as total_cases,
-	sum(cast(new_deaths as float)) as total_deaths,
-	(sum(cast(new_deaths as float))/nullif(sum(cast(new_cases as float)),0))*100 as PercentDeathvsInfected
+--- Death count vs Infected count
+select (location),
+	sum(new_cases) as total_cases,
+	sum(new_deaths) as total_deaths,
+	(sum(new_deaths)/nullif(sum(new_cases),0))*100 as PercentDeathvsInfected
 from covid_deaths
 where continent != ' '
 group by location
 order by 4 desc
 
----
-select
-	sum(cast(new_cases as float)) as total_cases,
-	sum(cast(new_deaths as float)) as total_deaths,
-	(sum(cast(new_deaths as float))/nullif(sum(cast(new_cases as float)),0))*100 as PercentDeathvsInfected
-from covid_deaths
-where continent != ' '
-
 --- What is the amount of People that were vaccination wrt total population
 select *
 from
 	covid_vaccination
+--- Changing the datatype of some columns in the dataset
+alter table covid_vaccination
+alter column date date
+go
+
+alter table covid_vaccination
+alter column new_vaccinations float
+go
+
 ---
-select cd.continent, cd.location, cast(cd.date as date) as date, cd.population, cv.new_vaccinations
+select cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations
 from covid_deaths as cd
 join covid_vaccination as cv
 	on cd.location = cv.location
@@ -167,13 +178,13 @@ join covid_vaccination as cv
 where cd.continent != ' ' 
 order by 1,2,3
 
----using rolling count
+---using 'Partition by' to calculate the cummulative count of vaccinations by location
 select cd.continent,
 	cd.location,
-	cast(cd.date as date) as date, 
+	cd.date, 
 	cd.population, 
 	cv.new_vaccinations, 
-	sum(cast(cv.new_vaccinations as float)) over (partition by cd.location order by cd.location,cast(cd.date as date)) as cummulativeVaccCount
+	sum(cv.new_vaccinations) over (partition by cd.location order by cd.location,cd.date) as cummulativeVaccCount
 from covid_deaths as cd
 join covid_vaccination as cv
 	on cd.location=cv.location
@@ -187,10 +198,10 @@ With cumm_table(continent, location, date, population, new_vaccinations,cumm_vac
 as
 (select cd.continent,
 	cd.location,
-	cast(cd.date as date) as date, 
-	cast(cd.population as float) as population, 
+	cd.date, 
+	cd.population, 
 	cv.new_vaccinations, 
-	sum(cast(cv.new_vaccinations as float)) over (partition by cd.location order by cd.location,cast(cd.date as date)) as cummulativeVaccCount
+	sum(cv.new_vaccinations) over (partition by cd.location order by cd.location, cd.date) as cummulativeVaccCount
 from covid_deaths as cd
 join covid_vaccination as cv
 	on cd.location=cv.location
@@ -198,7 +209,7 @@ join covid_vaccination as cv
 where cd.continent != ' '
 --and cd.location like '%united states%'
 )
-select *,(cumm_vac_count/nullif(population,0))*100
+select *,(cumm_vac_count/nullif(population,0)) *100 as PercentPopluationVaccinated
 from cumm_table
 order by 1,2,3
 
@@ -217,10 +228,10 @@ cumm_people_vaccinated float
 insert into #Percent_PeopleVaccinated
 select cd.continent,
 	cd.location,
-	cast(cd.date as date) as date, 
-	cast(cd.population as float) as population, 
+	cd.date, 
+	cd.population, 
 	cv.new_vaccinations, 
-	sum(cast(cv.new_vaccinations as float)) over (partition by cd.location order by cd.location,cast(cd.date as date)) as cummulativeVaccCount
+	sum(cv.new_vaccinations) over (partition by cd.location order by cd.location,cd.date) as cummulativeVaccCount
 from covid_deaths as cd
 join covid_vaccination as cv
 	on cd.location=cv.location
@@ -231,16 +242,17 @@ select *, (cumm_people_vaccinated/nullif(population,0)*100) as PercentPopulation
 from #Percent_PeopleVaccinated
 order by 2,3
 
+
 --- creating view to store data for Visualization
 
-create view PercentPopulationVaccinated as
+create view PercentPopVaccinated as
 
 select cd.continent,
 	cd.location,
-	cast(cd.date as date) as date, 
-	cast(cd.population as float) as population, 
+	cd.date, 
+	cd.population, 
 	cv.new_vaccinations, 
-	sum(cast(cv.new_vaccinations as float)) over (partition by cd.location order by cd.location,cast(cd.date as date)) as cummulativeVaccCount
+	sum(cv.new_vaccinations) over (partition by cd.location order by cd.location,cd.date) as cummulativeVaccCount
 from covid_deaths as cd
 join covid_vaccination as cv
 	on cd.location=cv.location
@@ -248,4 +260,4 @@ join covid_vaccination as cv
 where cd.continent != ' '
 
 select *
-from PercentPopulationVaccinated
+from PercentPopVaccinated
